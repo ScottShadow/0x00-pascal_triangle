b0fb8ff5f27fde4c26157e095b4c9f7d8d15631c
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-16 21:59:29 +0200

    initial commit

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+
+
+"""
+
+import sys
+from time import sleep
+
+
+def print_stats(stats_p: dict, file_size_p: int) -> None:
+    """
+    Prints the statistics calculated.
+    Args:
+        stats_p: Dictionary containing status code counts.
+        file_size_p: Total file size.
+    Returns:
+        None
+    """
+    print(f"File size: {file_size_p}")
+    for k, v in sorted(stats_p.items()):
+        if v:
+            print(f"{k}: {v}")
+
+
+if __name__ == '__main__':
+
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        sleep(1)
+        print_stats(stats, filesize)
+        raise
