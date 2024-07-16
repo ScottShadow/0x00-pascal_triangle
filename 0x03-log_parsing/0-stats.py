0c0f92ed6df578a18a3f9ec4fc7b1db46efcf67a
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-16 22:06:26 +0200

    minor fixes 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,27 +6,25 @@ and file sizes.
 import sys
 from time import sleep
 
-
-def print_stats(stats_p: dict, file_size_p: int) -> None:
-    """
-    Prints the statistics calculated.
-    Args:
-        stats_p: Dictionary containing status code counts.
-        file_size_p: Total file size.
-    Returns:
-        None
-    """
-    print(f"File size: {file_size_p}")
-    for k, v in sorted(stats_p.items()):
-        if v:
-            print(f"{k}: {v}")
-
-
 if __name__ == '__main__':
 
     filesize, count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
+
+    def print_stats(stats_p: dict, file_size_p: int) -> None:
+        """
+        Prints the statistics calculated.
+        Args:
+            stats_p: Dictionary containing status code counts.
+            file_size_p: Total file size.
+        Returns:
+            None
+        """
+        print(f"File size: {file_size_p}")
+        for k, v in sorted(stats_p.items()):
+            if v:
+                print(f"{k}: {v}")
 
     try:
         for line in sys.stdin:
