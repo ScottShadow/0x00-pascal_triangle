7590d0eeb25d2e565d0a14c86ebecf1ae82114d3
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-08 15:51:58 +0200

    linter fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -26,17 +26,18 @@ request({ url: url, json: true }, (error, response, body) => {
     // Log the parsed JSON data
     my_characters = body.characters;
     for (let i = 0; i < my_characters.length; i++) {
-      request({ url: my_characters[i], json: true }, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-          return;
-        }
-        // Check if the response status code is 200 (OK)
-        if (response.statusCode === 200) {
-          // Log the parsed JSON data
-          console.log(body.name);
-        }
-      })
+      request({ url: my_characters[i], json: true },
+        (error, response, body) => {
+          if (error) {
+            console.error('Error:', error);
+            return;
+          }
+          // Check if the response status code is 200 (OK)
+          if (response.statusCode === 200) {
+            // Log the parsed JSON data
+            console.log(body.name);
+          }
+        })
     }
   } else {
     console.log(`Failed to retrieve data: ${response.statusCode}`);
